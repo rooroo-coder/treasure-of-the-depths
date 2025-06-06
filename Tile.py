452c8fd767579b9fac83c0a7b8cceff3c5a22278
Winterking6198 <rowan3611@gmail.com>
2025-06-06 18:54:34 -0500

    add tile class

---
+++ Tile.py
@@ -0,0 +1,19 @@
+import Main
+
+X, Y = 0, 0
+img = 0
+overlay = "none"
+
+def __init__(self, X, Y, img):
+    self.X = X
+    self.Y = Y
+    self.img = img
+
+def getPixelX(self):
+    return ((self.X - Main.playerX) * Main.cell_size) + Main.offsetX
+
+def getPixelY(self):
+    return ((self.Y - Main.playerY) * Main.cell_size) + Main.offsetY
+
+def draw(self):
+    Main.window.blit(self.img, (getPixelX(self.X), getPixelY(self.Y)))
